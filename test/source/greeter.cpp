a01ea300dc02430bbcf8e67245635bf5fe379b79
riken127 <henriquenoronha05@gmail.com>
2024-08-03 19:21:38 +0100

    chore(version): changed the version and fixed the test.

--- test/source/greeter.cpp
+++ test/source/greeter.cpp
@@ -16,6 +16,6 @@ TEST_CASE("DataMesh") {
 }
 
 TEST_CASE("DataMesh version") {
-  static_assert(std::string_view(DATAMESH_VERSION) == std::string_view("1.0"));
-  CHECK(std::string(DATAMESH_VERSION) == std::string("1.0"));
+  static_assert(std::string_view(DATAMESH_VERSION) == std::string_view("0.0.1"));
+  CHECK(std::string(DATAMESH_VERSION) == std::string("0.0.1"));
 }
